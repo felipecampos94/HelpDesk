e11afde8f8120b3c35df5f60b5c7df13bdfe0814
felipecampos94 <felipecamposlv@gmail.com>
2022-03-31 21:29:48 -0300

    Update Chamado

--- src/main/java/com/projeto/helpdesk/service/ChamadoService.java
+++ src/main/java/com/projeto/helpdesk/service/ChamadoService.java
@@ -42,6 +42,13 @@ public class ChamadoService {
 		return repository.save(newChamado(objDTO));
 	}
 
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
